74dab3f55376a84a71a3d2bde408cb6e9d4d9ca8
Gerard Choinka <gerard.choinka@ambrosys.de>
2015-06-29 05:33:30 +0200

    add git pull test

--- UnitTest/SpeedTestDepth.sh
+++ UnitTest/SpeedTestDepth.sh
@@ -3,10 +3,11 @@
 TEST_SOURCE_REPO_URL=file:///tmp/bin_repo
 TEST_SOURCE_REPO_PATH=/tmp/bin_repo
 
-TEST_DST_REPO_PATH=/tmp/dst_repo
-TEST_DSTDEPTH_REPO_PATH=/tmp/dst_repo_depth
+TEST_DST_REPO_PATH=/tmp/dst_repo_full
+TEST_DST_DEPTH_REPO_PATH=/tmp/dst_repo_depth
+TEST_DST_PULL_REPO_PATH=/tmp/dst_repo_pull
 
-for D in ${TEST_SOURCE_REPO_PATH} ${TEST_DST_REPO_PATH} ${TEST_DSTDEPTH_REPO_PATH}
+for D in ${TEST_SOURCE_REPO_PATH} ${TEST_DST_REPO_PATH} ${TEST_DST_DEPTH_REPO_PATH} ${TEST_DST_PULL_REPO_PATH}
 do
     test -d "${D}" && rm -rf "${D}"
 done
@@ -16,7 +17,10 @@ git init
 git config user.email "you@example.com"
 git config user.name "Your Name"
 
-SIZE=1GiB
+git clone "${TEST_SOURCE_REPO_URL}" ${TEST_DST_PULL_REPO_PATH}
+
+
+SIZE=100MiB
 for((i=0; i < 3; ++i))
 do
     echo -e "\n######Run No. $i"
@@ -30,11 +34,20 @@ do
     time (git add random_file.bin && git commit -m "commit$i" )
     echo -e "\n######\n"
 
+    for D in ${TEST_DST_REPO_PATH} ${TEST_DST_DEPTH_REPO_PATH}
+    do
+        test -d "${D}" && rm -rf "${D}"
+    done
+    
     echo -e "\n######\n# normal clone full repo\n"
     time git clone "${TEST_SOURCE_REPO_URL}" "${TEST_DST_REPO_PATH}"
     echo -e "\n######\n"
 
     echo -e "\n######\n# depth 1 clone, only last revision\n"
-    time git clone --depth 1 "${TEST_SOURCE_REPO_URL}" "${TEST_DSTDEPTH_REPO_PATH}"
+    time git clone --depth 1 "${TEST_SOURCE_REPO_URL}" "${TEST_DST_DEPTH_REPO_PATH}"
+    echo -e "\n######\n"
+    
+    echo -e "\n######\n# pull, only last revision\n"
+    time git --git-dir=${TEST_DST_PULL_REPO_PATH}/.git --work-tree=${TEST_DST_PULL_REPO_PATH} pull "${TEST_SOURCE_REPO_URL}"
     echo -e "\n######\n"
 done
